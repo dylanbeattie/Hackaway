d70b0e71a23b8781e4ae31e5d525a7c3176e1871
Dylan Beattie <dylan@dylanbeattie.net>
2023-10-08 12:15:57 +0100

    Homepage, privacy policy, and tests

--- Rockaway/Rockaway.WebApp.Tests/PagesTests.cs
+++ Rockaway/Rockaway.WebApp.Tests/PagesTests.cs
@@ -1,13 +1,30 @@
-using Microsoft.AspNetCore.Mvc.Testing;
+using AngleSharp;
+using AngleSharp.Dom;
+using Shouldly;
 
 namespace Rockaway.WebApp.Tests;
 
 public class PagesTests {
-	[Fact]
-	public async Task Index_Page_Returns_Success() {
+	[Theory]
+	[InlineData("/")]
+	[InlineData("/privacy")]
+	public async Task Page_Returns_Success(string url) {
 		var factory = new WebApplicationFactory<Program>();
 		var client = factory.CreateClient();
-		var result = await client.GetAsync("/");
+		var result = await client.GetAsync(url);
 		result.EnsureSuccessStatusCode();
+	}
+
+	protected IBrowsingContext browsingContext => BrowsingContext.New(Configuration.Default);
+
+	[Fact]
+	public async Task Homepage_Title_Has_Correct_Content() {
+		var factory = new WebApplicationFactory<Program>();
+		var client = factory.CreateClient();
+		var html = await client.GetStringAsync("/");
+		var dom = await browsingContext.OpenAsync(req => req.Content(html));
+		var title = dom.QuerySelector("title");
+		title.ShouldNotBeNull();
+		title.InnerHtml.ShouldBe("Rockaway");
 	}
 }
